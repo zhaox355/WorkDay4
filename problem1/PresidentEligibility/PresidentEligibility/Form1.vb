9e4a20b9ed77279d9f83e7df0a88dce4121c8407
idsc3101 <idsc3101@gmail.com>
2013-09-23 15:23:47 -0500

    1

--- problem1/PresidentEligibility/PresidentEligibility/Form1.vb
+++ problem1/PresidentEligibility/PresidentEligibility/Form1.vb
@@ -3,6 +3,9 @@
 Public Class Form1
 
     Private Sub btnEligibility_Click(sender As System.Object, e As System.EventArgs) Handles btnEligibility.Click
-        
+        If radBornYes.Checked Then
+        ElseIf radBornNo.Checked Then
+
+        End If
     End Sub
 End Class
